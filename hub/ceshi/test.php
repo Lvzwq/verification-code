115746e7d9f775e2646a5affc9c925474eb46ed5
zwq <zhangwenqiang6@qq.com>
2014-09-08 14:12:33 +0800

    update sql

--- hub/ceshi/test.php
+++ hub/ceshi/test.php
@@ -1,54 +1,120 @@
 <?php
 
 include ('Valite.php');
-
-$valite = new Valite();
-$valite->setImage('../../hub11.jpg');
-$valite->getHec();
-$ert = $valite->run();
-$valite->Draw();
-//$ert = "1234";
-print_r($ert);
-
-
-function 
-
-
-//echo '<br><img src="abc.jpeg"><br>';
-/*
-$res = imagecreatefromjpeg("4.jpeg");
-		$size = getimagesize("4.jpeg");
-		$data = array();
-		for($i=0; $i < $size[1]; ++$i)
-		{
-			for($j=0; $j < $size[0]; ++$j)
-			{
-				$rgb = imagecolorat($res,$j,$i);
-				$rgbarray = imagecolorsforindex($res, $rgb);
-				
-				//var_dump($a);
-				//var_dump($rgbarray );
-				if($rgbarray['red'] <25  || $rgbarray['green']<25
-				|| $rgbarray['blue']< 25||$rgbarray['red'] >150  || $rgbarray['green']>150
-				|| $rgbarray['blue']>150)
-				{
-					$data[$i][$j]='-';
-				}else{
-					$data[$i][$j]=1;
-				}
-			}
-		}
-
-foreach($data as $value)
+/*发送登录请求*/
+function hublogin($username,$password)
 {
-	 // foreach($value as $v)
-	 // {
-		// echo $v;
-	 // }
-		echo implode("", $value);
-	 
-	 echo "<br />";
+	
+	$cookie=get_cookie();
+	$time=time()."123";
+	$key=get_key($username,$cookie,$time);
+	$code=get_code($username,$key,$time);
+	// $postdata=array(
+	// 	  'usertype' => 'xs',
+	// 	  'username' => 'U201210787' ,
+	// 	  'password' => 'endxNzY0MjEwM2x5',
+	// 	  'rand' => '45818',
+	// 	  'ln' => 'app68.dc.hust.edu.cn',
+	// 	  'random_key1' => '285300',
+	// 	  'random_key2' => 'd133697aa4ac60da2c0e057da4ffeec5',
+	// 	  'submit' => '立即登录'
+	// 	);
+	//var_dump($postdata);
+	$postdata=array(
+		'usertype'    =>"xs",
+		'username'    =>$username,
+		'password'    =>$password,
+		'rand'	      =>$code,
+		'ln'	      =>'app68.dc.hust.edu.cn',  
+		'random_key1' =>$key[0],
+		'random_key2' =>$key[1],
+		'submit'	  =>'立即登录'
+		);
+	var_dump($postdata);
+	$url="http://hub.hust.edu.cn/hublogin.action";
+	$referer="http://hub.hust.edu.cn/index.jsp";
+	$header=array(
+		'Connection'=>' keep-alive',
+		'Cache-Control'=>' no-cache',
+		'Accept'=>'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'Pragma'=>' no-cache',
+		'User-Agent'=>'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
+		'Accept-Language'=>'zh-CN,zh;q=0.8'
+		);
+	$cookie=$cookie = dirname(__FILE__)."/valid.tmp";
+	$curl=curl_init($url);
+	curl_setopt($curl, CURLOPT_REFERER, $referer);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_COOKIEFILE, $cookie);
+   // curl_setopt($curl, CURLOPT_COOKIEJAR, $cookie);
+    curl_setopt($curl, CURLOPT_POST,1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata);
+    $aCURLinfo = curl_getInfo( $curl );
+    // curl_setopt_array($curl, $options);
+    $info = curl_exec($curl);
+	curl_close($curl);
+	var_dump($info);
+	var_dump($aCURLinfo);
 }
-*/
-		
 
+/*获得cookie*/
+function get_cookie()
+{
+	$url = "http://hub.hust.edu.cn/index.jsp";
+	$cookie = dirname(__FILE__)."/valid.tmp";
+	//$cookie='';
+	$curl = curl_init($url);
+	//curl_setopt($curl, CURLOPT_REFERER, $referer);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($curl, CURLOPT_COOKIEFILE, $cookie);
+	curl_setopt($curl, CURLOPT_COOKIEJAR, $cookie);
+	$data = curl_exec($curl);
+	curl_close($curl);
+	return $cookie;	
+}
+
+/*获得key1和key2*/
+function get_key($username,$cookie,$date)
+{
+	$json_url="http://hub.hust.edu.cn/randomKey.action?username=".$username."&time=".$date;
+	$referer="http://hub.hust.edu.cn/index.jsp";
+	$curl = curl_init($json_url);
+    curl_setopt($curl, CURLOPT_REFERER, $referer);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_COOKIEFILE, $cookie);
+    $key = curl_exec($curl);
+	curl_close($curl);
+	return json_decode($key);
+}
+
+function get_code($username,$msg,$date)
+{
+	/*保存验证码为本地图片*/
+	$cookie = dirname(__FILE__)."/valid.tmp";
+	$date=time()."123";
+	$code_url="http://hub.hust.edu.cn/randomImage.action?k1=".$msg[0]."&k2=".$msg[1]."&uno=".$username."&time=$date";
+
+	$curl = curl_init($code_url);
+	//curl_setopt($curl, CURLOPT_REFERER, $referer);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($curl, CURLOPT_COOKIEFILE, $cookie);
+	curl_setopt($curl, CURLOPT_COOKIEJAR, $cookie);
+	$data = curl_exec($curl);
+	curl_close($curl);
+
+	$fp = fopen("code.jpg","wb");
+	fwrite($fp, $data);
+	fclose($fp);
+	/*识别验证码图片的数字*/
+	$valite = new Valite();
+	$valite->setImage('code.jpg');
+	$valite->getHec();
+	$code = $valite->run();
+	return $code;
+}
+
+hublogin("U201210787","endxNzY0MjEwM2x5");
+var_dump($_POST);
+
+ 
